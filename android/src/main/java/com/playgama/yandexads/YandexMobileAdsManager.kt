e7ec5b6ab9797b591b1c4e6e88b165d1f7c9d8f7
EdikN <84894638+EdikN@users.noreply.github.com>
2026-05-15 15:37:24 +0500

    fix: rewrite for Yandex Mobile Ads 8.0 coroutines API, compileSdk 36

--- android/src/main/java/com/playgama/yandexads/YandexMobileAdsManager.kt
+++ android/src/main/java/com/playgama/yandexads/YandexMobileAdsManager.kt
@@ -1,9 +1,9 @@
 package com.playgama.yandexads
 
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.getcapacitor.JSObject
 import com.yandex.mobile.ads.banner.BannerAdEventListener
 import com.yandex.mobile.ads.banner.BannerAdSize
@@ -11,34 +11,35 @@ import com.yandex.mobile.ads.banner.BannerAdView
 import com.yandex.mobile.ads.common.AdError
 import com.yandex.mobile.ads.common.AdRequest
 import com.yandex.mobile.ads.common.AdRequestConfiguration
-import com.yandex.mobile.ads.common.AdRequestError
 import com.yandex.mobile.ads.common.ImpressionData
 import com.yandex.mobile.ads.common.MobileAds
 import com.yandex.mobile.ads.interstitial.InterstitialAd
 import com.yandex.mobile.ads.interstitial.InterstitialAdEventListener
-import com.yandex.mobile.ads.interstitial.InterstitialAdLoadListener
 import com.yandex.mobile.ads.interstitial.InterstitialAdLoader
 import com.yandex.mobile.ads.rewarded.Reward
 import com.yandex.mobile.ads.rewarded.RewardedAd
 import com.yandex.mobile.ads.rewarded.RewardedAdEventListener
-import com.yandex.mobile.ads.rewarded.RewardedAdLoadListener
 import com.yandex.mobile.ads.rewarded.RewardedAdLoader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class YandexMobileAdsManager(
     private val activity: AppCompatActivity,
     private val plugin: YandexMobileAdsPlugin,
 ) {
     private val bannerViews = mutableMapOf<String, BannerAdView>()
-    private val adRequest = AdRequest.Builder().build()
+    private val scope = CoroutineScope(Dispatchers.Main)
 
     fun initialize(appMetricaKey: String?) {
         MobileAds.initialize(activity) {}
     }
 
     fun showInterstitial(adUnitId: String, callback: (String?) -> Unit) {
-        val loader = InterstitialAdLoader(activity)
-        loader.setAdLoadListener(object : InterstitialAdLoadListener {
-            override fun onAdLoaded(ad: InterstitialAd) {
+        scope.launch {
+            try {
+                val loader = InterstitialAdLoader(activity)
+                val ad = loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
                 ad.setAdEventListener(object : InterstitialAdEventListener {
                     override fun onAdShown() {
                         plugin.emit("interstitialOpened")
@@ -54,30 +55,28 @@ class YandexMobileAdsManager(
                     override fun onAdClicked() {}
                     override fun onAdImpression(data: ImpressionData?) {}
                 })
-                activity.runOnUiThread { ad.show(activity) }
+                ad.show(activity)
                 callback(null)
+            } catch (e: Exception) {
+                plugin.emit("interstitialFailed", JSObject().put("error", e.message))
+                callback(e.message)
             }
-            override fun onAdFailedToLoad(error: AdRequestError) {
-                plugin.emit("interstitialFailed", JSObject().put("error", error.description))
-                callback(error.description)
-            }
-        })
-        loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+        }
     }
 
     fun preloadInterstitial(adUnitId: String) {
-        val loader = InterstitialAdLoader(activity)
-        loader.setAdLoadListener(object : InterstitialAdLoadListener {
-            override fun onAdLoaded(ad: InterstitialAd) {}
-            override fun onAdFailedToLoad(error: AdRequestError) {}
-        })
-        loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+        scope.launch {
+            try {
+                InterstitialAdLoader(activity).loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+            } catch (_: Exception) {}
+        }
     }
 
     fun showRewarded(adUnitId: String, callback: (String?) -> Unit) {
-        val loader = RewardedAdLoader(activity)
-        loader.setAdLoadListener(object : RewardedAdLoadListener {
-            override fun onAdLoaded(ad: RewardedAd) {
+        scope.launch {
+            try {
+                val loader = RewardedAdLoader(activity)
+                val ad = loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
                 ad.setAdEventListener(object : RewardedAdEventListener {
                     override fun onAdShown() {
                         plugin.emit("rewardedOpened")
@@ -96,44 +95,39 @@ class YandexMobileAdsManager(
                     override fun onAdClicked() {}
                     override fun onAdImpression(data: ImpressionData?) {}
                 })
-                activity.runOnUiThread { ad.show(activity) }
+                ad.show(activity)
                 callback(null)
+            } catch (e: Exception) {
+                plugin.emit("rewardedFailed", JSObject().put("error", e.message))
+                callback(e.message)
             }
-            override fun onAdFailedToLoad(error: AdRequestError) {
-                plugin.emit("rewardedFailed", JSObject().put("error", error.description))
-                callback(error.description)
-            }
-        })
-        loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+        }
     }
 
     fun preloadRewarded(adUnitId: String) {
-        val loader = RewardedAdLoader(activity)
-        loader.setAdLoadListener(object : RewardedAdLoadListener {
-            override fun onAdLoaded(ad: RewardedAd) {}
-            override fun onAdFailedToLoad(error: AdRequestError) {}
-        })
-        loader.loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+        scope.launch {
+            try {
+                RewardedAdLoader(activity).loadAd(AdRequestConfiguration.Builder(adUnitId).build())
+            } catch (_: Exception) {}
+        }
     }
 
     fun showBanner(adUnitId: String, position: String, callback: (String?) -> Unit) {
         activity.runOnUiThread {
             try {
                 val bannerAdView = BannerAdView(activity)
-                bannerAdView.setAdUnitId(adUnitId)
-                bannerAdView.setAdSize(BannerAdSize.stickySize(activity, 320))
+                bannerAdView.adUnitId = adUnitId
+                bannerAdView.adSize = BannerAdSize.fixedSize(activity, 320, 50)
                 bannerAdView.setBannerAdEventListener(object : BannerAdEventListener {
                     override fun onAdLoaded() {
                         plugin.emit("bannerShown")
                         callback(null)
                     }
-                    override fun onAdFailedToLoad(error: AdRequestError) {
-                        plugin.emit("bannerFailed", JSObject().put("error", error.description))
-                        callback(error.description)
+                    override fun onAdFailedToLoad(adError: AdError) {
+                        plugin.emit("bannerFailed", JSObject().put("error", adError.description))
+                        callback(adError.description)
                     }
                     override fun onAdClicked() {}
-                    override fun onLeftApplication() {}
-                    override fun onReturnedToApplication() {}
                     override fun onImpression(data: ImpressionData?) {}
                 })
 
@@ -144,7 +138,7 @@ class YandexMobileAdsManager(
                     if (position == "top") Gravity.TOP else Gravity.BOTTOM,
                 )
                 rootView.addView(bannerAdView, params)
-                bannerAdView.loadAd(adRequest)
+                bannerAdView.loadAd(AdRequest.Builder().build())
                 bannerViews[adUnitId] = bannerAdView
             } catch (e: Exception) {
                 callback(e.message)
